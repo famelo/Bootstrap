74f098a4e478080324b6fbd95c38d2e073e1b622
Marc Neuhaus <apocalip@gmail.com>
2013-08-27 01:29:39 +0200

    Removed jquery form minified js

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -1,4 +1,3 @@
-//@codekit-prepend "../Components/jquery/jquery.js"
 //@codekit-prepend "../Components/bootstrap/js/affix.js"
 //@codekit-prepend "../Components/bootstrap/js/alert.js"
 //@codekit-prepend "../Components/bootstrap/js/button.js"
@@ -6,10 +5,10 @@
 //@codekit-prepend "../Components/bootstrap/js/collapse.js"
 //@codekit-prepend "../Components/bootstrap/js/dropdown.js"
 //@codekit-prepend "../Components/bootstrap/js/modal.js"
+//@codekit-prepend "../Components/bootstrap/js/tooltip.js"
 //@codekit-prepend "../Components/bootstrap/js/popover.js"
 //@codekit-prepend "../Components/bootstrap/js/scrollspy.js"
 //@codekit-prepend "../Components/bootstrap/js/tab.js"
-//@codekit-prepend "../Components/bootstrap/js/tooltip.js"
 //@codekit-prepend "../Components/bootstrap/js/transition.js"
 
 $(document).ready(function() {
